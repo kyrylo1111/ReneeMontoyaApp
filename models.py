2fbd74c51d9047ccf5c13dc7c2e30c0853e1a024
kyrylo1111 <96440229+kyrylo1111@users.noreply.github.com>
2021-12-22 18:51:03 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,74 @@
 from abc import ABC, abstractmethod
 import json
 import pymysql
-
+from database.config import host, user, password, db_name
 class Model(ABC):
     file = 'default.json'
 
+    def savep(self):
+
+        try:
+            connection = pymysql.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+
+            )
+        except Exception:
+            print("соси хуй")
+        else:
+            print("все гуд")
+        mycursor = connection.cursor()
+
+        sql = "INSERT INTO Plants (id, location, name, director_id) VALUES (%s, %s, %s, %s)"
+        val = (self.id, self.location, self.name, self.director_id)
+        mycursor.execute(sql, val)
+
+        connection.commit()
+
+    def savee(self):
+        try:
+            connection = pymysql.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+
+            )
+        except Exception:
+            print("соси хуй")
+        else:
+            print("все гуд")
+        mycursor = connection.cursor()
+
+        sql1 = "INSERT INTO Employees (id, name, email, department_type, department_id) VALUES (%s, %s, %s, %s, %s)"
+        employee = (self.id, self.name, self.email, self.department_type, self.department_id)
+        mycursor.execute(sql1, employee)
+
+        connection.commit()
+
+    def saves(self):
+        try:
+            connection = pymysql.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+
+            )
+        except Exception:
+            print("соси хуй")
+        else:
+            print("все гуд")
+        mycursor = connection.cursor()
+
+        sql2 = "INSERT INTO Salons (id, name, email, department_type, department_id, salon) VALUES (%s, %s, %s, %s, %s, %s)"
+        salo = (self.id, self.name, self.email, self.department_type, self.department_id, self.salon)
+
+        mycursor.execute(sql2, salo)
+
+        connection.commit()
     def save(self):
         file_in_dict_format = self._generate_dict()
         file = self.get_file_data(self.file)
